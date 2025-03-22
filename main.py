d31a16f348f5e45b73e54554e44d5f5db22cac3e
crummei <56043602+crummei@users.noreply.github.com>
2025-03-22 21:34:15 +0100

    Update main.py
    Added an X hours cooldown for delay

--- main.py
+++ main.py
@@ -175,7 +175,9 @@ async def start_timer(message, channel, guild_id):
     else:
         delay = advertChannels[guild_id][4] if len(advertChannels[guild_id]) > 4 else defaultDelay
 
-    logging.info(f"{YELLOW}Starting delay for{RESET} {guild_id}{YELLOW}:{RESET} {delay // 60}{YELLOW} minutes")
+    logging.info(f"{YELLOW}Starting delay for{RESET} {guild_id}{YELLOW}:{RESET} "
+             f"{delay // 60}{YELLOW} minutes{RESET}" if delay // 60 <= 60 
+             else f"{(delay // 60) // 60}{YELLOW} hours{RESET}")
     await asyncio.sleep(delay)
     bot.timers[guild_id] = False
     channel_id, allows_invites, allows_markdown, allows_emojis, *_ = advertChannels[guild_id]
