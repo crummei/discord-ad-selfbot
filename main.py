5788bdb43eadea0a1364e4a9d305650463463348
crummei <56043602+crummei@users.noreply.github.com>
2025-03-24 13:13:01 +0100

    Update main.py
    Enabled Yota advertising. Lvl 3 reached

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ advertChannels = {
     152517096104919042:     (296693573292916741,    False,      True,       True,           216100+rand.randint(5,10)),     # Official RL Server
     677907326568628247:     (748545565243211776,    True,       True,       True            ),          # Striped
     681994761787146253:     (699019104228475004,    True,       True,       True,           7200+rand.randint(5,10)),      # Uuest
-    # 1061789478508843109:    (1074743282212544643,   False,      False,      True            ),          # Yota (Need lvl 3 to promo)
+    1061789478508843109:    (1074743282212544643,   False,      False,      True            ),          # Yota (Need lvl 3 to promo)
     689614991770517517:     (715709356796280832,    True,       True,       True            ),          # CBell
     489971613312221214:     (519668945930813440,    False,      True,       False,          1800+rand.randint(5,10)),      # Musty (Phone verification required)
     303678101726953473:     (333234568188526592,    False,      True,       True,           1800+rand.randint(5,10)),      # Sunless
